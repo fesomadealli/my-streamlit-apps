a07d36bed68067a3202b47c1752b4462930fb95d
fesomadealli <fesomade.alli@gmail.com>
2023-11-17 02:06:51 +0100

    Update 3_debugger.py

--- pages/3_debugger.py
+++ pages/3_debugger.py
@@ -114,165 +114,166 @@ if plot_results is True:
     # Data for the donut chart
     total_games_played = cum_df['count'].sum()
 
-#     #  Setting up Figure & Axes
-#     fig, ax = plt.subplots(1,3, figsize=(12, 4), facecolor=facecolor, constrained_layout=True)
+    #  Setting up Figure & Axes
+    fig, ax = plt.subplots(1,3, figsize=(12, 4), facecolor=facecolor, constrained_layout=True)
 
-#     # dictionary to access the contents of the Cumulative Results Column
-#     emp = {}
-#     for i in range(len(cum_df)):   #Can't put this block of code in the second for loop because we need  
-#         emp[cum_df['index'][i]] = cum_df[f'{select_period}'][i] #to fill the dctionary first before accessing it.
+    # dictionary to access the contents of the Cumulative Results Column
+    emp = {}
+    for i in range(len(cum_df)):   #Can't put this block of code in the second for loop because we need  
+        emp[cum_df[f'{select_period}'][i]] = cum_df['count'][i] #to fill the dctionary first before accessing it.
+    
+    emp
 
-
-#     for i in range(len(cum_df)):
-#         if order[i] == 'W':
-#             colors = [ax_color, plot_color]
-#             num_wins = str(emp[order[i]])
-#         if order[i] == 'D':
-#             colors = [ax_color, plot_color]
-#             num_draws = str(emp[order[i]])
-#         if order[i] == 'L':
-#             colors = [ax_color, plot_color]
-#             num_loss = str(emp[order[i]])
-#         ax[i].set_yticks([])
-#         ax[i].set_xticks([])
-#         ax[i].set_facecolor(ax_color)
-#         hide_spines(axes=ax[i], which_spine="all")
+    for i in range(len(cum_df)):
+        if order[i] == 'W':
+            colors = [ax_color, plot_color]
+            num_wins = str(emp[order[i]])
+        if order[i] == 'D':
+            colors = [ax_color, plot_color]
+            num_draws = str(emp[order[i]])
+        if order[i] == 'L':
+            colors = [ax_color, plot_color]
+            num_loss = str(emp[order[i]])
+        ax[i].set_yticks([])
+        ax[i].set_xticks([])
+        ax[i].set_facecolor(ax_color)
+        plot_functions.hide_spines(axes=ax[i], which_spine="all")
        
-#         # Calculate % of that portion/selection
-#         pct = (emp[order[i]] / total_games_played) * 100 
-#         # Create the donut chart
-#         ax[i].pie([total_games_played-emp[order[i]], emp[order[i]]], colors=colors,
-#                   startangle=90, wedgeprops={'width': 1.5})
-#         # Highlight the variable of interest in the center
-#         center_circle = plt.Circle((0, 0), 0.62, color=facecolor)
-#         ax[i].add_artist(center_circle)
-#         # Add the corresponding percentage text in the center
-#         # Pct Labels
-#         h_axs(ax=ax[i], x=-.38, y=.05, s=f'{pct: .2f}%', color=off_white, 
-#               font=t_font, fontsize=18, fontweight='bold', zorder=2)
+        # Calculate % of that portion/selection
+        pct = (emp[order[i]] / total_games_played) * 100 
+        # Create the donut chart
+        ax[i].pie([total_games_played-emp[order[i]], emp[order[i]]], colors=colors,
+                  startangle=90, wedgeprops={'width': 1.5})
+        # Highlight the variable of interest in the center
+        center_circle = plt.Circle((0, 0), 0.62, color=facecolor)
+        ax[i].add_artist(center_circle)
+        # Add the corresponding percentage text in the center
+        # Pct Labels
+        h_axs(ax=ax[i], x=-.38, y=.05, s=f'{pct: .2f}%', color=off_white, 
+              font=t_font, fontsize=18, fontweight='bold', zorder=2)
 
-#     # Wins% Label
-#     h_axs(ax=ax[0], x=-.23, y=-.15, s='<Win %>', color=off_white, 
-#           highlight_textprops=[{'color':plot_color}], font=b_font,
-#           fontsize=b_fsize, fontweight='bold', zorder=2)
-#     # Draws% Label
-#     h_axs(ax=ax[1], x=-.21, y=-.15, s='<Draw %>', color=off_white,
-#           highlight_textprops=[{'color':plot_color}], font=b_font,
-#           fontsize=b_fsize, fontweight='bold', zorder=2) 
-#     # Loss% Label
-#     h_axs(ax=ax[2], x=-.21, y=-.15, s='<Loss %>', color=off_white,
-#           highlight_textprops=[{'color':plot_color}], font=b_font,
-#           fontsize=b_fsize, fontweight='bold', zorder=2)
+    # Wins% Label
+    h_axs(ax=ax[0], x=-.23, y=-.15, s='<Win %>', color=off_white, 
+          highlight_textprops=[{'color':plot_color}], font=b_font,
+          fontsize=b_fsize, fontweight='bold', zorder=2)
+    # Draws% Label
+    h_axs(ax=ax[1], x=-.21, y=-.15, s='<Draw %>', color=off_white,
+          highlight_textprops=[{'color':plot_color}], font=b_font,
+          fontsize=b_fsize, fontweight='bold', zorder=2) 
+    # Loss% Label
+    h_axs(ax=ax[2], x=-.21, y=-.15, s='<Loss %>', color=off_white,
+          highlight_textprops=[{'color':plot_color}], font=b_font,
+          fontsize=b_fsize, fontweight='bold', zorder=2)
 
-#     # Title Text x-location on Figure if the Opponent Name is to be printed on the Title
-#     title_xloc=.25
-#     ttl_space = hspace*4
-#     oppnt_name_char = len(select_opponent)
-#     if select_opponent != 'All_teams':
-#         if select_opponent != 'Successful Christian Mission FC':
-#             if oppnt_name_char>3 & oppnt_name_char<=15:
-#                 title_xloc=.21
-#                 ttl_space = hspace*5
-#         else: 
-#             select_opponent = 'Successful CMFC'
-#             title_xloc=.21
-#             ttl_space = hspace*10
+    # Title Text x-location on Figure if the Opponent Name is to be printed on the Title
+    title_xloc=.25
+    ttl_space = hspace*4
+    oppnt_name_char = len(select_opponent)
+    if select_opponent != 'All_teams':
+        if select_opponent != 'Successful Christian Mission FC':
+            if oppnt_name_char>3 & oppnt_name_char<=15:
+                title_xloc=.21
+                ttl_space = hspace*5
+        else: 
+            select_opponent = 'Successful CMFC'
+            title_xloc=.21
+            ttl_space = hspace*10
 
-#     # Title Text
-#     title_text=" "
-#     if select_period == 'FTR':
-#         if select_opponent != 'All_teams':
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <FT MATCH RESULTS%> vs. {select_opponent.upper()} IN HOME GAMES\n"
-#                               f"{ttl_space}UNDER {coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                 title_text = (f"{team} <FT MATCH RESULTS%> vs. {select_opponent.upper()} IN ALL MEETINGS\n"
-#                               f"{ttl_space}UNDER {coach.upper()} ({data_span})")
-#         # No Particular Opponent Selected        
-#         elif select_opponent == "All_teams":
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <FT MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
-#                               f"{hspace*8}{coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                 title_text = f"{team} <FT MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
-#                 title_xloc=.14
+    # Title Text
+    title_text=" "
+    if select_period == 'FTR':
+        if select_opponent != 'All_teams':
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <FT MATCH RESULTS%> vs. {select_opponent.upper()} IN HOME GAMES\n"
+                              f"{ttl_space}UNDER {coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                title_text = (f"{team} <FT MATCH RESULTS%> vs. {select_opponent.upper()} IN ALL MEETINGS\n"
+                              f"{ttl_space}UNDER {coach.upper()} ({data_span})")
+        # No Particular Opponent Selected        
+        elif select_opponent == "All_teams":
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <FT MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
+                              f"{hspace*8}{coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                title_text = f"{team} <FT MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
+                title_xloc=.14
 
-#     if select_period == 'HTR':
-#         if select_opponent != 'All_teams':
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <HT MATCH RESULTS%> vs. {select_opponent.upper()} IN HOME GAMES\n"
-#                               f"{ttl_space}UNDER {coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                 title_text = (f"{team} <HT MATCH RESULTS%> vs. {select_opponent.upper()} IN ALL MEETINGS\n"
-#                               f"{ttl_space}UNDER {coach.upper()} ({data_span})")
-#         # No Particular Opponent Selected        
-#         elif select_opponent == "All_teams":
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <HT MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
-#                               f"{hspace*8}{coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                 title_text = f"{team} <HT MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
-#                 title_xloc=.14
+    if select_period == 'HTR':
+        if select_opponent != 'All_teams':
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <HT MATCH RESULTS%> vs. {select_opponent.upper()} IN HOME GAMES\n"
+                              f"{ttl_space}UNDER {coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                title_text = (f"{team} <HT MATCH RESULTS%> vs. {select_opponent.upper()} IN ALL MEETINGS\n"
+                              f"{ttl_space}UNDER {coach.upper()} ({data_span})")
+        # No Particular Opponent Selected        
+        elif select_opponent == "All_teams":
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <HT MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
+                              f"{hspace*8}{coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                title_text = f"{team} <HT MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
+                title_xloc=.14
 
-#     if select_period == 'SHR':
-#         if select_opponent != 'All_teams':
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <2ND HALF MATCH RESULTS%> vs. {select_opponent.upper()}"
-#                               f" IN HOME GAMES\n{ttl_space}UNDER {coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                 title_text = (f"{team} <2ND HALF MATCH RESULTS%> vs. {select_opponent.upper()}"
-#                               f" IN ALL MEETINGS\n{ttl_space}UNDER {coach.upper()} ({data_span})")
-#         # No Particular Opponent Selected       
-#         elif select_opponent == "All_teams":
-#             if select_range == 'Home_Games':
-#                 title_text = (f"{team} <2ND HALF MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
-#                               f"{hspace*8}{coach.upper()} ({data_span})")
-#             elif select_range == "All_Games":
-#                     title_text = f"{team} <2ND HALF MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
-#                     title_xloc=.14
+    if select_period == 'SHR':
+        if select_opponent != 'All_teams':
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <2ND HALF MATCH RESULTS%> vs. {select_opponent.upper()}"
+                              f" IN HOME GAMES\n{ttl_space}UNDER {coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                title_text = (f"{team} <2ND HALF MATCH RESULTS%> vs. {select_opponent.upper()}"
+                              f" IN ALL MEETINGS\n{ttl_space}UNDER {coach.upper()} ({data_span})")
+        # No Particular Opponent Selected       
+        elif select_opponent == "All_teams":
+            if select_range == 'Home_Games':
+                title_text = (f"{team} <2ND HALF MATCH RESULTS%> IN ALL HOME GAMES UNDER\n"
+                              f"{hspace*8}{coach.upper()} ({data_span})")
+            elif select_range == "All_Games":
+                    title_text = f"{team} <2ND HALF MATCH RESULTS%> UNDER {coach.upper()} ({data_span})"
+                    title_xloc=.14
 
-#     # Set titles for the figure and the subplot respectively
-#     h_fig(x=title_xloc, y=1.18, s=title_text, color=off_white, highlight_textprops=[{'color':plot_color}],
-#            font=t_font, fontsize=t_fsize, fontweight='bold', zorder=2)
+    # Set titles for the figure and the subplot respectively
+    h_fig(x=title_xloc, y=1.18, s=title_text, color=off_white, highlight_textprops=[{'color':plot_color}],
+           font=t_font, fontsize=t_fsize, fontweight='bold', zorder=2)
 
-#     # FT Results Breakdown
-#     txt = f"<Games Played:> " + str(len(df)) + f" <| Wins:> {num_wins}" + f" <| Draws:> {num_draws}" + f" <| Loss:> {num_loss}"
+    # FT Results Breakdown
+    txt = f"<Games Played:> " + str(len(df)) + f" <| Wins:> {num_wins}" + f" <| Draws:> {num_draws}" + f" <| Loss:> {num_loss}"
 
-#     h_fig(x=.22, y=-.08, s=txt, color=off_white, highlight_textprops=[{'color':plot_color}, 
-#                                                                       {'color':plot_color},
-#                                                                       {'color':plot_color},
-#                                                                       {'color':plot_color}],
-#            font=t_font, fontsize=t_fsize, fontweight='bold', zorder=2)
+    h_fig(x=.22, y=-.08, s=txt, color=off_white, highlight_textprops=[{'color':plot_color}, 
+                                                                      {'color':plot_color},
+                                                                      {'color':plot_color},
+                                                                      {'color':plot_color}],
+           font=t_font, fontsize=t_fsize, fontweight='bold', zorder=2)
 
-#     # Endnotes
-#     gms_incl = get_comps(df)
-#     endnote_text = (f"Matches Included: {gms_incl}\n" 
-#                     f"{add_note}")
-#     endnote = fig.text(x=0.51, y= -.38, s=endnote_text, color=off_white, linespacing= 2,
-#                        ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
+    # Endnotes
+    gms_incl = get_comps(df)
+    endnote_text = (f"Matches Included: {gms_incl}\n" 
+                    f"{add_note}")
+    endnote = fig.text(x=0.51, y= -.38, s=endnote_text, color=off_white, linespacing= 2,
+                       ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
 
-#     # Name Text
-#     name_text = fig.text(x=0.51, y= -.55, s=author, ha='center', va='center', 
-#                        font=b_font, color=plot_color, alpha=.3, fontsize=b_fsize, zorder=2)
+    # Name Text
+    name_text = fig.text(x=0.51, y= -.55, s=author, ha='center', va='center', 
+                       font=b_font, color=plot_color, alpha=.3, fontsize=b_fsize, zorder=2)
 
-#     # Path Effects for texts
-#     endnote.set_path_effects([path_effects.Stroke(linewidth=.018, foreground=off_white),
-#                               path_effects.Normal()])
-#     name_text.set_path_effects([path_effects.Stroke(linewidth=.005, foreground=plot_color), 
-#                                 path_effects.Normal()])
+    # Path Effects for texts
+    endnote.set_path_effects([path_effects.Stroke(linewidth=.018, foreground=off_white),
+                              path_effects.Normal()])
+    name_text.set_path_effects([path_effects.Stroke(linewidth=.005, foreground=plot_color), 
+                                path_effects.Normal()])
 
-#     # Figure Paddings         
-#     fig.text(x=0.19, y= 1.20, s=pad_top, color=off_white, linespacing= 2,
-#                     ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
+    # Figure Paddings         
+    fig.text(x=0.19, y= 1.20, s=pad_top, color=off_white, linespacing= 2,
+                    ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
 
-#     fig.text(x=0.51, y= -.50, s=pad_end, color=off_white, linespacing= 2,
-#                     ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
+    fig.text(x=0.51, y= -.50, s=pad_end, color=off_white, linespacing= 2,
+                    ha='center', va='center', font=b_font, fontsize=b_fsize, zorder=2)
 
-#     # plt.tight_layout()
-#     return fig
+    # plt.tight_layout()
+    return fig
 
-# else:
-#     error_msg = ("No Plot to Display")
-#     return error_msg
-# fig
+else:
+    error_msg = ("No Plot to Display")
+    return error_msg
+fig
 # return fig
