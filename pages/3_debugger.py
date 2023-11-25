cb044c8eb22f31a25aec603f388bb18c4750b5b9
fesomadealli <fesomade.alli@gmail.com>
2023-11-25 01:57:35 +0100

    Update 3_debugger.py
    Improving Hyperlinks

--- pages/3_debugger.py
+++ pages/3_debugger.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import random
-import time
-import quiz
 
 # newline char
 def nl(num_of_lines):
@@ -32,40 +29,4 @@ def redirect_button(url: str, text: str= None, color="#da111b"):
     """,
     unsafe_allow_html=True
     )
-redirect_button("import streamlit as st
-import random
-import time
-import quiz
-
-# newline char
-def nl(num_of_lines):
-    for i in range(num_of_lines):
-        st.write(" ")
-#  Add lines
-def add_line(val=False):
-    if val == True:
-        st.markdown('''
-        ---
-                    ''')
-
-# Page Title
-def redirect_button(url: str, text: str= None, color="#da111b"):
-    st.markdown(
-    f"""
-    <a href="{url}" target="_self">
-        <div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: white;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>
-    </a>
-    """,
-    unsafe_allow_html=True
-    )
-redirect_button("http://stackoverflow.com","this leads to SO")
-","BORDERLINE SYNDROME")
-
+redirect_button("https://medium.com/p/c04792872e46/edit","THE BORDERLINE SYNDROME")
