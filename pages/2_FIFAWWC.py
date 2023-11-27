6953eb38ee448de1de021af5d684d014cd200ab3
fesomadealli <fesomade.alli@gmail.com>
2023-11-27 10:11:04 +0100

    Update 2_FIFAWWC.py

--- pages/2_FIFAWWC.py
+++ pages/2_FIFAWWC.py
@@ -1,39 +1,5 @@
-# import streamlit as st
+st.header("**WORK IN PROGRESS**")
 
-# # Sidebar navigation
-# st.sidebar.title("Navigation")
-# pages = ["Home", "About", "Analytics"]
-# selected_page = st.sidebar.selectbox("Select Page", pages)
-
-# # Define the layout for each page
-# def home_page():
-#     st.subheader("Welcome to the Home Page")
-#     st.write("This is the Home Page. You can learn more about this app here.")
-
-# def about_page():
-#     st.subheader("About")
-#     st.write("This is the About page. You can find information about us here.")
-
-# def analytics_page():
-#     st.subheader("Analytics")
-#     selected_subpage = st.sidebar.selectbox("Select Analytics Subpage", analytics_subpages)
-#     st.title(f"Analytics - {selected_subpage}")
-#     if selected_subpage == "Bar Charts":
-#         st.write("Content for Bar Charts goes here.")
-#     elif selected_subpage == "Pie Charts":
-#         st.write("Content for Pie Charts goes here.")
-#     elif selected_subpage == "Podium Plots":
-#         st.write("Content for Podium Plots goes here.")
-#     elif selected_subpage == "Time Series":
-#         st.write("Content for Time Series goes here.")
-
-# # Subpages under "Analytics"
-# analytics_subpages = ["Bar Charts", "Pie Charts", "Podium Plots", "Time Series"]
-
-# # Display the selected page's layout
-# if selected_page == "Home":
-#     home_page()
-# elif selected_page == "About":
-#     about_page()
-# elif selected_page == "Analytics":
-#     analytics_page()
+st.markdown("""
+This App would enable users to analytically view every player who participated in the 2023 FIFAWWC in AU-NZ by examining player-level detail. The analysis would be based on Statsbomb FREE data for the Women's World Cup. the project was inspired by Ana Beatriz Macedo. You can have a look at her work [here](https://statsbombchartapp-anabeatrizmacedo.streamlit.app/)
+""")
