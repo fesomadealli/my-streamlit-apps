4b37966fd9f5d6ed429c8b631109dda1a01b7e5e
fesomadealli <fesomade.alli@gmail.com>
2023-11-27 10:50:40 +0100

    Update 2_FIFAWWC.py

--- pages/2_FIFAWWC.py
+++ pages/2_FIFAWWC.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header("### **WORK IN PROGRESS**")
+st.header("**WORK IN PROGRESS**")
 
 st.markdown("""
 This App would enable users to analytically view every player who participated in the 2023 FIFAWWC in AU-NZ by examining player-level detail. The analysis would be based on Statsbomb FREE data for the Women's World Cup. \nThe project was inspired by Ana Beatriz Macedo. You can have a look at her work [here](https://statsbombchartapp-anabeatrizmacedo.streamlit.app/)
